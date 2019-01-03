fa7536550dc410184e767daae78ff09bf8961868
sugesh2016 <sugesh.somarajan@msn.com>
2019-01-03 12:47:48 +0530

    Correction

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -23,7 +23,7 @@ namespace GradeBook.GradeBooks
             else if (grades[(threshold * 2)] - 1 <= averageGrade)
                 return 'B';
             else if (grades[(threshold * 3)] - 1 <= averageGrade)
-                return 'c';
+                return 'C';
             else if (grades[(threshold * 4)] - 1 <= averageGrade)
                 return 'D';
             else return 'F';
