16fa4b62178a6abd488445e53c7016b230557c46
Elliott Jointer <ejointer@bexar.org>
2019-01-11 15:29:14 -0600

    Added BaseGradeBook

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,14 @@
+﻿
+
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
---
+++ GradeBook/RankedGradeBook.cs
@@ -0,0 +1,13 @@
+﻿using GradeBook.Enums;
+using GradeBook.GradeBooks;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
