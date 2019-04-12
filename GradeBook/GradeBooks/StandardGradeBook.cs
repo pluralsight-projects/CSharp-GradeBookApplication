c472c248cb29082bde3c56529353dfd07ad0100a
BobaBrett902 <49533631+BobaBrett902@users.noreply.github.com>
2019-04-12 11:14:14 -0500

    Add files via upload

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Name = name;
+            Type = GradeBookType.Standard;
+        }
+
+    }
+}
