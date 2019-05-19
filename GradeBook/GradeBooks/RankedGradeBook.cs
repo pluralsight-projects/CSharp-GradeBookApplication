33d6e8656e7eb36c977dc72df77ed391ef473f36
Oldteach74 <49858519+Oldteach74@users.noreply.github.com>
2019-05-19 18:35:43 +0800

    and Again

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -49,8 +49,8 @@ namespace GradeBook.GradeBooks
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
             }
-            else
-            {
+        
+            
                 base.CalculateStudentStatistics(name);
             }
         }
