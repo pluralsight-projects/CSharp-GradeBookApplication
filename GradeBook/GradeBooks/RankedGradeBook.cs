9194826ab8498d1ee68f286a062240608f8af9ba
Oldteach74 <49858519+Oldteach74@users.noreply.github.com>
2019-05-19 18:32:01 +0800

    again

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,7 +12,7 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if (Students.Count > 5)
+            if(Students.Count > 5)
             {
                 var threshold = (int)Math.Ceiling(Students.Count * 0.2);
                 var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
@@ -31,7 +31,7 @@ namespace GradeBook.GradeBooks
         }
         public override void CalculateStatistics()
         {
-            if (Students.Count < 5)
+            if(Students.Count < 5)
             {
                 base.CalculateStatistics();
             }
@@ -44,13 +44,15 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStudentStatistics(string name)
         {
-            if (Students.Count < 5)
+            if(Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
             }
-
-            base.CalculateStudentStatistics(name);
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
         }
     }
 }
