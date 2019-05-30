a48f8f6b895ec691257c47668f88d4cda11e6364
Zach Rebstock <zachary_rebstock@yahoo.com>
2019-05-30 08:52:45 -0500

    oops

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
    public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) :base(name)
+        public RankedGradeBook(string name) : base(name)
         {
-            Type = Enums.GradeBookType.Ranked;
+            Type = GradeBookType.Ranked;
         }
         public override char GetLetterGrade(double averageGrade)
         {
