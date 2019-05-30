4455fad0d05f3985e9fd966431271d8082c8a82f
Zach Rebstock <zachary_rebstock@yahoo.com>
2019-05-30 08:39:42 -0500

    update

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+   public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) :base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            return base.GetLetterGrade(averageGrade);
+        }
+    }
+}
