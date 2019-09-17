4c377883f1ac702e5c6a197881109aa811e6a2ca
247benji <42581204+247benji@users.noreply.github.com>
2019-09-17 16:21:53 +0100

    Set ranked grade boundaries

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using System.Linq;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,40 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
+
+            var band = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[band - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (grades[(band * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[(band * 3) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[(band * 4) - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
+
         }
     }
 }
