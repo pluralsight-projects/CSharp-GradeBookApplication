3d1250ceb2c99e5eade719d7a8ac49d2d44cde71
Joseph Carlson <joecar1111@aol.com>
2019-09-29 21:32:50 -0400

    chcek my answer

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,8 +16,14 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException();
             }
-            return 'F';
+            if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade >= 60)
+                return 'B';
+            else if (averageGrade >= 40)
+                return 'C';
+            else if (averageGrade >= 20)
+                return 'D';
         }
-
     }
 }
