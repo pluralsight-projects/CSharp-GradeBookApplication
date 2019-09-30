718d41836cbfe6b4ffc783a728b0ae5a504bc0aa
Joseph Carlson <joecar1111@aol.com>
2019-09-29 23:26:26 -0400

    test

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,16 +41,18 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2].ToLower();
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            BaseGradeBook gradeBook;
             if (type == "standard")
                 gradeBook = new StandardGradeBook(name);
             else if (type == "ranked")
                 gradeBook = new RankedGradeBook(name);
             else
             {
-                Console.WriteLine(type + " is not a supported type of gradebook, please try again");    
+                Console.WriteLine(type + " is not a supported type of gradebook, please try again");
+                return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
+            GradeBookUserInterface.CommandLoop(gradeBook);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
