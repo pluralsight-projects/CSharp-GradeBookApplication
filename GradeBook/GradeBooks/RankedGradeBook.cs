56313d9a069173ccdf85a2f5ac0e02d0f14a45d1
247benji <42581204+247benji@users.noreply.github.com>
2019-10-01 15:45:35 +0100

    Missing work found

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
+
+            var band = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[band - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (grades[(band * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[(band * 3) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[(band * 4) - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
+
+        }
+    }
+}
