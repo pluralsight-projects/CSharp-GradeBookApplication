8694c4bc23a0f6680be3d3fedb661f76d3a371b4
247benji <42581204+247benji@users.noreply.github.com>
2019-10-01 15:54:41 +0100

    Create CalculateStudentStatistics Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -57,5 +57,17 @@ namespace GradeBook.GradeBooks
             }
         }
 
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+
     }
 }
