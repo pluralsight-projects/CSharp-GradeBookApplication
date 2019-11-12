91a255e88334c10a5fe141f1e0efe1b25e456fcd
erickg-magenic <erickg@magenic.com>
2019-11-13 04:15:38 +0800

    Removed bracketing for pluralsight checking.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,20 +14,19 @@ namespace GradeBook.GradeBooks {
             }
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
-
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if(grades[threshold - 1] <= averageGrade) {
+            if(grades[threshold - 1] <= averageGrade)
                 return 'A';
-            } else if(grades[(threshold*2) - 1] <= averageGrade) {
+            else if(grades[(threshold * 2) - 1] <= averageGrade)
                 return 'B';
-            } else if(grades[(threshold*3) - 1] <= averageGrade) {
+            else if(grades[(threshold * 3) - 1] <= averageGrade)
                 return 'C';
-            } else if(grades[(threshold*4) - 1] <= averageGrade) {
+            else if(grades[(threshold * 4) - 1] <= averageGrade)
                 return 'D';
-            } else {
+            else
                 return 'F';
-            }
+            
         }
     }
 }
