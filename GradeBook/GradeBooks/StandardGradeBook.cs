f53bb8f18ad138f066a07b887821c584215e04d6
erickg-magenic <erickg@magenic.com>
2019-11-13 01:36:08 +0800

    Added new files.

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,10 @@
+using System;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks {
+    public class StandardGradeBook : BaseGradeBook {
+        public StandardGradeBook(string name) : base(name) {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
