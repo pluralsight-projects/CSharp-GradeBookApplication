9e212b936bd7a3aa114bc16df869ebfbfde144a6
Christy Trevino <crtrevino@gmail.com>
2020-05-17 10:35:25 -0500

    Create GradeBook.cs

---
+++ GradeBook/Enums/GradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+    public enum GradeBook
+    {
+        Standard, 
+        Ranked, 
+        ESNU,
+        OneToFour,
+        SixPoint
+    }
+}
