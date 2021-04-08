63989ac36faf081cafb8b0ed48b18a6b3f46ba39
mikael <mikaelrosendahl76@gmail.com>
2021-04-08 14:16:34 +0200

    corrected some issues

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -5,7 +5,7 @@ namespace GradeBook.GradeBooks
     public class RankedGradeBook : BaseGradeBook
     {
         // the constructor
-        public RankedGradeBook (string name) :  base(name)
+        public RankedGradeBook(string name) : base(name)
             {
                 Type = GradeBookType.Ranked;
             }
