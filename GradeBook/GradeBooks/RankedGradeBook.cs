5c550dade8ae26f247ada00e76cada384754755f
mikael <mikaelrosendahl76@gmail.com>
2021-04-08 14:59:35 +0200

    wrote the logic for grades

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,8 +10,28 @@ namespace GradeBook.GradeBooks
                 Type = GradeBookType.Ranked;
             }
         //override method
-       /* public override char GetLetterGrade (double avreagegrade)
-            return base.GetLetterGrade(avreagegrade);*/
+        public override char GetLetterGrade (double avreagegrade)
+
+            if(Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+
+                return base.GetLetterGrade(avreagegrade);
+
+        var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+        var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+        if(grades[threshold - 1] <= averageGrade)
+            return 'A';
+
+            else if(grades[threshold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if(grade[threshold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if(grade[threshold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
+
 
     }
 }
