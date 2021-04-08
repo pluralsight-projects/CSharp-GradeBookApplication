94945ed610b5ad1c65019d3bbde2bfd81794723e
mikael <mikaelrosendahl76@gmail.com>
2021-04-08 19:06:02 +0200

    added curlybrackets to method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -9,28 +9,30 @@ namespace GradeBook.GradeBooks
     {
         // the constructor
         public RankedGradeBook(string name) : base(name)
-            {
-                Type = GradeBookType.Ranked;
-            }
+        {
+            Type = GradeBookType.Ranked;
+        }
         //override method
-        //public override char GetLetterGrade(double averageGrade);
+        public override char GetLetterGrade(double averageGrade)
+        {
 
-        //    if(Students.Count < 5)
-        //        throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
 
 
-        //var threshold = (int)Math.Ceiling(Students.Count * 0.2);
-        //var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-        //    if (grades[threshold - 1] <= averageGrade)
-        //        return 'A';
-        //    else if (grades[threshold * 2) -1] <= averageGrade)
-        //        return 'B';
-        //    else if (grades[threshold * 3) -1] <= averageGrade)
-        //        return 'C';
-        //    else if (grades[threshold * 4) -1] <= averageGrade)
-        //        return 'D';
-        //    else
-        //        return 'F';
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[threshold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[threshold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[threshold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
+        }
     }
 }
