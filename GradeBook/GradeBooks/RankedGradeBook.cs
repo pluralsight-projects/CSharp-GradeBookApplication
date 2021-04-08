b456d781a6417850b3462680e4146454e3966a6f
mikael <mikaelrosendahl76@gmail.com>
2021-04-08 09:42:49 +0200

    Created method GetLetterGrade to overide.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,6 +4,8 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
+        GetLetterGrade();
+
         public RankedGradeBook (string name) :  base(name)
             {
                 Type = GradeBookType.Ranked;
