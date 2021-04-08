45c3487ae56970287011451930a60c4b7be34c48
mikael <mikaelrosendahl76@gmail.com>
2021-04-08 18:57:10 +0200

    messing around

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,24 +13,24 @@ namespace GradeBook.GradeBooks
                 Type = GradeBookType.Ranked;
             }
         //override method
-       /* public override char GetLetterGrade(double averageGrade)
+        //public override char GetLetterGrade(double averageGrade);
 
-            if(Students.Count < 5)
-                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
-                
+        //    if(Students.Count < 5)
+        //        throw new InvalidOperationException("Ranked grading requires at least 5 students.");
 
-        var threshold = (int)Math.Ceiling(Students.Count * 0.2);
-        var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if(grades[threshold - 1] <= averageGrade)
-                return 'A';
-            else if(grades[threshold * 2) - 1] <= averageGrade)
-                return 'B';
-            else if(grades[threshold * 3) - 1] <= averageGrade)
-                return 'C';
-            else if(grades[threshold * 4) - 1] <= averageGrade)
-                return 'D';
-            else
-                return 'F';*/
+        //var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+        //var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+        //    if (grades[threshold - 1] <= averageGrade)
+        //        return 'A';
+        //    else if (grades[threshold * 2) -1] <= averageGrade)
+        //        return 'B';
+        //    else if (grades[threshold * 3) -1] <= averageGrade)
+        //        return 'C';
+        //    else if (grades[threshold * 4) -1] <= averageGrade)
+        //        return 'D';
+        //    else
+        //        return 'F';
     }
 }
